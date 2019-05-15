85409c9941a277d5f8834d63e4465df1b0c02f81
Ewen Lai <lwwens@gmail.com;lai_wei_wen@qq.com>
2019-05-15 17:49:31 +0800

    Site updated: 2019-05-15 17:49:31

--- js/dist/music.js
+++ js/dist/music.js
@@ -1,3 +1,3 @@
-// build time:Wed May 15 2019 17:42:59 GMT+0800 (China Standard Time)
+// build time:Wed May 15 2019 17:49:30 GMT+0800 (China Standard Time)
 const ap=new APlayer({container:document.getElementById("aplayer"),autoplay:true,loop:"all",order:"random",preload:"auto",volume:.7,listFolded:true,listMaxHeight:60,lrcType:3,mutex:true,audio:[{name:"Stay Here Forever",artist:"Jewel",url:"http://www.ytmp3.cn/down/50472.mp3",cover:"/images/musics/Jewel.jpg"},{name:"富士山下 (Live)",artist:"陈奕迅",url:"http://m10.music.126.net/20190515180134/8fea9d6b4198069db65a3068978c745e/ymusic/dc8a/8a68/bf64/d461a501e630907bb611a849c7d6735d.mp3",cover:"/images/musics/tianxiawushuang.jpg"}]});
 //rebuild by neat 

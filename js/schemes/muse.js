d8272bf5fc4a885603f2f207ab9ce7771e3230cc
Ewen Lai <lwwens@gmail.com;lai_wei_wen@qq.com>
2019-05-17 23:57:30 +0800

    Site updated: 2019-05-17 23:57:30

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Fri May 17 2019 23:53:31 GMT+0800 (China Standard Time)
+// build time:Fri May 17 2019 23:57:29 GMT+0800 (China Standard Time)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 

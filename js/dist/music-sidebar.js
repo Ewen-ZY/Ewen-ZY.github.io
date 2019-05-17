4262364fcddca34210fb533a2142e0b32dec3dbe
Ewen Lai <lwwens@gmail.com;lai_wei_wen@qq.com>
2019-05-17 23:53:03 +0800

    Site updated: 2019-05-17 23:53:03

--- js/dist/music-sidebar.js
+++ js/dist/music-sidebar.js
@@ -1,3 +1,3 @@
-// build time:Thu May 16 2019 11:20:27 GMT+0800 (China Standard Time)
+// build time:Fri May 17 2019 23:53:02 GMT+0800 (China Standard Time)
 const ap=new APlayer({container:document.getElementById("aplayer"),autoplay:false,loop:"all",order:"random",preload:"auto",volume:.7,listFolded:true,listMaxHeight:60,lrcType:3,mutex:true,audio:[{name:"Stay Here Forever",artist:"Jewel",url:"http://www.ytmp3.cn/down/50472.mp3",cover:"/images/musics/Jewel.jpg"},{name:"天下无双 (Live In Hong Kong, 2013)",artist:"陈奕迅",url:"http://m10.music.126.net/20190515234416/e0e73301684f420d1eb83c5f96db091b/ymusic/dc8a/8a68/bf64/d461a501e630907bb611a849c7d6735d.mp3",cover:"/images/musics/tianxiawushuang.jpg"},{name:"两个女孩",artist:"莫文蔚",url:"http://m10.music.126.net/20190515234742/86d59e052fdd4ec3a52ffbcce78c6af2/ymusic/4850/b74d/d35d/6a8d6dfa7bc5baaa677f1b23403ef15e.mp3",cover:"./images/musics/shierloudemowenwei.jpg",lrc:"./lrcs/sidebar/shierloudemowenwei.lrc"}]});
 //rebuild by neat 

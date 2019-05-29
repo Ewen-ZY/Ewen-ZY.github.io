1843c7611b7230da68eb51c19c30410bb816316e
Ewen Lai <lwwens@gmail.com;lai_wei_wen@qq.com>
2019-05-29 10:30:13 +0800

    Site updated: 2019-05-29 10:30:11

--- js/scroll-cookie.js
+++ js/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue May 28 2019 21:35:38 GMT+0800 (China Standard Time)
+// build time:Wed May 29 2019 10:30:11 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
